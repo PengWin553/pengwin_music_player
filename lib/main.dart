b287094015a15e1502a05bcf8759187860a3a56d
PengWin553 <victoriakobayashi553@gmail.com>
2025-04-03 12:01:27 +0800

    feat: navbar and temp body container

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Home(),
-  )
-);
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -33,9 +30,64 @@ class Home extends StatelessWidget {
             color: Colors.white,
           ),
         ],
-        
+
         backgroundColor: Colors.purple[900],
         foregroundColor: Colors.white,
+      ),
+
+      body: Column(
+        children: [
+          // Container with a background color for the Row
+          Container(
+            color: Colors.purple, // Set background color here
+            // padding: EdgeInsets.symmetric(vertical: 10), // Optional padding
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.deepPurple,
+                  child: Text('Songs', style: TextStyle(color: Colors.white)), // Color for individual items
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.deepPurple,
+                  child: Text('Playlists', style: TextStyle(color: Colors.white)),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.deepPurple,
+                  child: Text('Folders', style: TextStyle(color: Colors.white)),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.deepPurple,
+                  child: Text('Albums', style: TextStyle(color: Colors.white)),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  color: Colors.deepPurple,
+                  child: Text('Artists', style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+          ),
+
+          SizedBox(height: 10),
+
+          // Another Column
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.blue,
+                child: Text('Temp Container', style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
