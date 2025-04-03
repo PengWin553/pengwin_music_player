5447241dbc2c18eab2761c91243cd0f42605d50e
PengWin553 <victoriakobayashi553@gmail.com>
2025-04-03 16:26:50 +0800

    style: improve base ui with linear gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -3,91 +3,129 @@ import 'package:flutter/material.dart';
 void main() =>
     runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // title: Text('My App'), // Add a title if needed
-        leading: IconButton(
-          // left icon
-          onPressed: () {
-            // print('You clicked me, an IconButton.');
-          },
-          icon: Icon(Icons.menu),
-          color: Colors.white,
-        ),
+  State<Home> createState() => _HomeState();
+}
 
-        actions: [
-          // right-aligned icons
-          IconButton(
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Color.fromARGB(255, 52, 1, 114), Color.fromARGB(255, 126, 3, 156)],
+        ),
+      ),
+      child: Scaffold(
+        // By defaut, Scaffold background is white
+        // Set its value to transparent
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          // title: Text('My App'), // Add a title if needed
+          leading: IconButton(
+            // left icon
             onPressed: () {
               // print('You clicked me, an IconButton.');
             },
-            icon: Icon(Icons.search_sharp),
+            icon: Icon(Icons.menu),
             color: Colors.white,
           ),
-        ],
 
-        backgroundColor: Colors.purple[900],
-        foregroundColor: Colors.white,
-      ),
-
-      body: Column(
-        children: [
-          // Container with a background color for the Row
-          Container(
-            color: Colors.purple, // Set background color here
-            // padding: EdgeInsets.symmetric(vertical: 10), // Optional padding
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  color: Colors.deepPurple,
-                  child: Text('Songs', style: TextStyle(color: Colors.white)), // Color for individual items
-                ),
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  color: Colors.deepPurple,
-                  child: Text('Playlists', style: TextStyle(color: Colors.white)),
-                ),
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  color: Colors.deepPurple,
-                  child: Text('Folders', style: TextStyle(color: Colors.white)),
-                ),
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  color: Colors.deepPurple,
-                  child: Text('Albums', style: TextStyle(color: Colors.white)),
-                ),
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  color: Colors.deepPurple,
-                  child: Text('Artists', style: TextStyle(color: Colors.white)),
-                ),
-              ],
+          actions: [
+            // right-aligned icons
+            IconButton(
+              onPressed: () {
+                // print('You clicked me, an IconButton.');
+              },
+              icon: Icon(Icons.search_sharp),
+              color: Colors.white,
             ),
-          ),
+          ],
 
-          SizedBox(height: 10),
+          backgroundColor: Colors.transparent,
+          foregroundColor: Colors.white,
+        ),
 
-          // Another Column
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.blue,
-                child: Text('Temp Container', style: TextStyle(color: Colors.white)),
+        body: Column(
+          children: [
+            // Container with a background color for the Row
+            Container(
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.all(10.0),
+                      color: Colors.transparent,
+                      child: Text(
+                        'Songs',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                      ), // Color for individual items
+                    ),
+                  ),
+
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.all(10.0),
+                      color: Colors.transparent,
+                      child: Text(
+                        'Playlists',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.all(10.0),
+                      color: Colors.transparent,
+                      child: Text(
+                        'Folders',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.all(10.0),
+                      color: Colors.transparent,
+                      child: Text(
+                        'Albums',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.all(10.0),
+                      color: Colors.transparent,
+                      child: Text(
+                        'Artists',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
