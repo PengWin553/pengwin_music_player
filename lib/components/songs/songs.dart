3edfcb9736a68be23dfedb4f1f148d8759f2abe6
PengWin553 <victoriakobayashi553@gmail.com>
2025-04-04 14:53:17 +0800

    update: songs component base ui (compelete)
    designed for dynamic integration

--- lib/components/songs/songs.dart
+++ lib/components/songs/songs.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() =>
@@ -11,6 +12,14 @@ class Songs extends StatefulWidget {
 }
 
 class _SongsState extends State<Songs> {
+  List<Song> songs = [
+    Song(title: "Compared Child", artist: "TUYU"),
+    Song(title: "It's Raining After All", artist: "TUYU"),
+    Song(title: "Moonlight", artist: "Suisei"),
+    Song(title: "Tabun", artist: "Yoasobi"),
+    Song(title: "Gurenge", artist: "LiSa"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -20,7 +29,7 @@ class _SongsState extends State<Songs> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           // vertical alignment
           crossAxisAlignment: CrossAxisAlignment.end,
-        
+
           children: [
             Padding(
               padding: const EdgeInsets.all(12.0),
@@ -33,7 +42,7 @@ class _SongsState extends State<Songs> {
                 ),
               ),
             ),
-        
+
             Padding(
               padding: const EdgeInsets.all(4.0),
               child: Row(
@@ -51,7 +60,7 @@ class _SongsState extends State<Songs> {
                     color: Colors.white,
                     iconSize: 18.00,
                   ),
-              
+
                   IconButton(
                     onPressed: () {},
                     icon: Icon(Icons.checklist),
@@ -67,14 +76,94 @@ class _SongsState extends State<Songs> {
         Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              color: Colors.purple[900],
-              child: Text('This is the Container inside a column', style: TextStyle(color: Colors.white))
-            )
-          ],
-        )
+          children:
+              songs.map((song) {
+                return Container(
+                  padding: EdgeInsets.fromLTRB(10, 6, 10, 6),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // left container
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                Color.fromARGB(255, 43, 1, 92),
+                                Color.fromARGB(255, 126, 13, 154),
+                              ],
+                            ),
+                            border: Border.all(color: Colors.purple),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10.0),
+                            ),
+                          ),
+                          child: Icon(
+                            CupertinoIcons.music_note,
+                            size: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+
+                      // center container
+                      Expanded(
+                        flex: 8,
+                        child: Container(
+                          padding: EdgeInsets.fromLTRB(16, 5, 0, 5),
+                          child: Column(
+                            crossAxisAlignment:
+                                CrossAxisAlignment
+                                    .start, // This aligns children to the left
+                            children: [
+                              Text(
+                                song.title,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+
+                              Text(
+                                song.artist,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      // right container
+                      Expanded(
+                        flex: 1,
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.more_vert),
+                          color: Colors.white,
+                          iconSize: 18.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+        ),
       ],
     );
   }
+}
+
+// Song class
+class Song {
+  final String title;
+  final String artist;
+
+  Song({required this.title, required this.artist});
 }
